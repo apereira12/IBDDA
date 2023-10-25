52655c3bc5853367a33c798a28e9331b1551d258
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 01:12:37 -0300

    Update extra013.py

--- extra013.py
+++ extra013.py
@@ -1,2 +1,12 @@
 #Gerador de Senhas
 #Solicite ao usuário o comprimento desejado da senha e gere uma senha aleatória desse tamanho.
+import string
+import random
+
+def gerador(tamanho):
+    caracteres = string.ascii_letters + string.digits
+    senha = random.choices(caracteres, k=tamanho)
+    return ''.join(senha)
+
+tamanho = int(input('Favor informar o tamanho da senha:'))
+print(gerador(tamanho))
